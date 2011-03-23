a0f2233c343074852de682b830cf9dc5744a0f6c
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2011-03-23 23:37:00 +0300

    Some fixes

---
+++ common/script/index.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm $EO_ROOT/frontend/index/*
+indexer --all -c exorg.sphinx
--- frontend/http_server/script/run.sh
+++ frontend/http_server/script/run.sh
@@ -2,5 +2,7 @@
 
 cd $EO_ROOT/frontend/http_server/src/config
 
+searchd -c $EO_ROOT/common/script/exorg.sphinx
+
 java -cp $EO_CP \
      eo.common.Starter config.xml
