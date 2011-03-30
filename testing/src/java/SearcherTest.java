592aec6a7ddab4a07808759fe449d7d92d2a0008
Alexey Gurevich <alexeigurevich@gmail.com>
2011-03-31 02:21:52 +0400

    first drafts for testing

---
+++ testing/src/java/SearcherTest.java
@@ -0,0 +1,37 @@
+package eo.testing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alex
+ * Date: 3/30/11
+ * Time: 9:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import junit.framework.Assert.*;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SearcherTest {
+
+    @Test
+    public void testQueryById()
+    {
+        try {
+            assertEquals(eo.frontend.httpserver.Searcher.queryById(1).name, "Исаакиевский собор");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testQueryTypes()
+    {
+        try {
+            assertEquals(eo.frontend.httpserver.Searcher.queryTypes().length, 7);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
