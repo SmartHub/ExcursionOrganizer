f04bf59d6191808b6336d8690600d0141b2b9473
Alexey Gurevich <alexeigurevich@gmail.com>
2011-04-13 03:26:07 +0400

    some modification of sql tests

--- testing/sql/run.sh
+++ testing/sql/run.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-PWD=$(pwd)
+EO_ROOT=$(pwd)/../..
+
+username=$(awk '/jdbc.username/' $EO_ROOT/custom.properties | awk  -F= '{print $2}')
+password=$(awk '/jdbc.password/' $EO_ROOT/custom.properties | awk  -F= '{print $2}')
 
 echo "\nqualitative tests:\n"
-cat qualitative_tests.sql   | mysql -u exorg excursion_organizer
+if [ "$password" != "" ]
+	then
+		cat qualitative_tests.sql   | mysql -u $username -p $password excursion_organizer
+	else
+		cat qualitative_tests.sql   | mysql -u $username excursion_organizer
+fi
 echo "\nquantitative tests:\n"
-cat quantitative_tests.sql  | mysql -u exorg excursion_organizer
+if [ "$password" != "" ]
+	then
+		cat quantitative_tests.sql   | mysql -u $username -p $password excursion_organizer
+	else
+		cat quantitative_tests.sql   | mysql -u $username excursion_organizer
+fi
