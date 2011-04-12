5e4cf0391b5010dbc67f7ee843d502b3dd72c8a2
Alexey Gurevich <alexeigurevich@gmail.com>
2011-04-13 03:23:42 +0400

    some modification of sql tests

--- exorg/start.sh
+++ exorg/start.sh
@@ -13,9 +13,9 @@ EO_LIB=$EO_ROOT/lib
 EO_CP=$(echo $(find $EO_LIB -follow -name *jar) | sed 's/ /:/g')
 
 echo "run miner"
-#cd $EO_ROOT/miner/config
-#java -cp $EO_CP \
-#    eo.common.util.Starter config.xml $EO_ROOT
+cd $EO_ROOT/miner/config
+java -cp $EO_CP \
+    eo.common.util.Starter config.xml $EO_ROOT
 
 echo "run processing"
 #cd $EO_ROOT/processing/config
@@ -26,5 +26,5 @@ echo "index database"
 #$EO_ROOT/common/script/index.py $EO_ROOT $username $password
 
 echo "run server"
-cd $EO_ROOT
-./svr_local.sh start $EO_ROOT $EO_CP
+#cd $EO_ROOT
+#./svr_local.sh start $EO_ROOT $EO_CP
