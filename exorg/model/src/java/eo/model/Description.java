9c242f7b099932f7dd90f0aa410b0621f2d3d86b
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2011-04-15 00:44:06 +0400

    Integrity fix

---
+++ exorg/model/src/java/eo/model/Description.java
@@ -0,0 +1,23 @@
+package eo.model;
+
+import java.lang.*;
+
+// ================================================================================
+
+public final class Description {
+    private String text;
+    private String sourceUrl;
+    
+    public Description(final String t, final String s) {
+        this.text = t;
+        this.sourceUrl = s;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getSourceURL() {
+        return sourceUrl;
+    }
+}
---
+++ exorg/model/src/java/eo/model/Location.java
@@ -0,0 +1,76 @@
+package eo.model;
+
+// ================================================================================
+
+final public class Location {
+    private long cityId;
+    private String address;
+    private double lat;
+    private double lng;
+
+    public Location() { 
+        this.address = null;
+        this.lat = -1;
+        this.lng = -1;
+    }
+
+    public Location(final String address) {
+        this.address = address;
+        this.lat = -1;
+        this.lng = -1;
+    }
+
+    public Location(long cityId, final String address) {
+        this.cityId = cityId;
+        this.address = address;
+        this.lat = -1;
+        this.lng = -1;
+    }
+
+    public Location(long cityId, final String address, double lat, double lng) {
+        this.cityId = cityId;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    final public String getAddress() {
+        return this.address;
+    }
+
+    final public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    final public double getLat() {
+        return lat;
+    }
+
+    final public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    final public double getLng() {
+        return lng;
+    }
+
+    final public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    final public long getCityId() {
+        return this.cityId;
+    }
+
+    final public void setCityId(long cityId) {
+        this.cityId = cityId;
+    }
+
+    public boolean isValid() {
+        return this.lat != -1 && this.lng != -1;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+}
