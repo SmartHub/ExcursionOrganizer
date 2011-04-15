945d7e4b7ccbf22e0e93a69c62cf8d8663d8df5a
Svetlana <lana@lana-note.(none)>
2011-04-16 00:09:04 +0400

    module indes was added

---
+++ exorg/index/src/eo/index/Searcher.java
@@ -0,0 +1,18 @@
+package eo.index;
+
+import org.sphx.api.SphinxClient;
+import org.sphx.api.SphinxMatch;
+import org.sphx.api.SphinxResult;
+import org.springframework.jdbc.core.JdbcOperations;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lana
+ * Date: 15.04.11
+ * Time: 23:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Searcher {
+
+
+}
