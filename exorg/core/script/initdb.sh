e9c3cd6f2b222385db97d87da924f984ae11b930
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2011-04-30 02:51:00 +0400

    DB initialization fixed

--- exorg/core/script/initdb.sh
+++ exorg/core/script/initdb.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+cd $EO_ROOT/core/script
 if [ "$2" != "" ]
     then
-        cat $EO_ROOT/core/script/creation.sql | mysql -u $1 -p $2 excursion_organizer
+        cat creation.sql | mysql -u $1 -p $2 excursion_organizer
     else
-        cat $EO_ROOT/core/script/creation.sql | mysql -u $1 excursion_organizer
+        cat creation.sql | mysql -u $1 excursion_organizer
 fi
 
 cd $EO_ROOT
