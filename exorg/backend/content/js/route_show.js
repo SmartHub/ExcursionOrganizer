1c9560b76465659c65d171dfa151e44daac384be
Pavel Sinay <si---pay@yandex.ru>
2011-05-06 12:53:13 +0400

    route sorting works

--- exorg/backend/content/js/route_show.js
+++ exorg/backend/content/js/route_show.js
@@ -22,3 +22,37 @@ function init_points()
 
 	}, 'xml'); // указываем явно тип данных
 };
+
+function update_points()
+{
+	var PoiList = [];
+
+    $('input.number').each(function()
+    {
+        Poi =
+        {
+            Order : $(this).attr("value"),
+            Id : $(this).attr("poi-id"),
+            Name : $(this).attr('name'),
+            Lat : $(this).attr("lat"),
+            Lng : $(this).attr("lng")
+        };
+        //alert(Poi.Id + "; " + Poi.Name + " ; " + Poi.Lat + 3"; " + Poi.Lng);
+        PoiList.push(Poi);
+        //alert(Poi.Id + " " + Poi.Name + " " + Poi.Lat + " " + Poi.Lng);
+    });
+
+    PoiList.sort(Comparator);
+    calculate_route(PoiList);
+    /* в этом месте будет вызвана функция для отрисовки маршрута с тем же массивом объектов в кач-ве параметра */
+
+};
+
+function Comparator(i, ii) {
+    if (i.Order > ii.Order)
+        return -1;
+    else if (i.Order < ii.Order)
+        return 1;
+    else
+        return 0;
+}
--- exorg/backend/content/route.xsl
+++ exorg/backend/content/route.xsl
@@ -34,6 +34,18 @@
                         <xsl:attribute name="value">
                             <xsl:value-of select="@order"/>
                         </xsl:attribute>
+                        <xsl:attribute name="lat">
+                            <xsl:value-of select="@lat"/>
+                        </xsl:attribute>
+                        <xsl:attribute name="lng">
+                            <xsl:value-of select="@lng"/>
+                        </xsl:attribute>
+                        <xsl:attribute name="poi-id">
+                            <xsl:value-of select="@poi-id"/>
+                        </xsl:attribute>
+                        <xsl:attribute name="name">
+                            <xsl:value-of select="name"/>
+                        </xsl:attribute>
                     </input>
                     <!--input type="hidden">
                         <xsl:attribute name="value">
