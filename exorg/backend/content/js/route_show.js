202e66d77ec8130839893cbef82bf5c0223d431d
Pavel Sinay <si---pay@yandex.ru>
2011-05-06 13:02:15 +0400

    url in poi array

--- exorg/backend/content/js/route_show.js
+++ exorg/backend/content/js/route_show.js
@@ -11,12 +11,14 @@ function init_points()
 				Id : $(this).attr("poi-id"),
 				Name : $(this).find('name').text(),
 				Lat : $(this).attr("lat"),
-				Lng : $(this).attr("lng")
+				Lng : $(this).attr("lng"),
+				Url : "poi.html?id=" + $(this).attr("poi-id")
 			};
+			//Poi.Url = "poi.html?id=" + Poi.Id;
             //alert(Poi.Id + "; " + Poi.Name + " ; " + Poi.Lat + "; " + Poi.Lng);
 			PoiList.push(Poi);
         	});
-		
+		                                                             
 		calculate_route(PoiList);
 		/* в этом месте будет вызвана функция для отрисовки маршрута с тем же массивом объектов в кач-ве параметра */
 
@@ -35,11 +37,12 @@ function update_points()
             Id : $(this).attr("poi-id"),
             Name : $(this).attr('name'),
             Lat : $(this).attr("lat"),
-            Lng : $(this).attr("lng")
+            Lng : $(this).attr("lng"),
+            Url : "poi.html?id=" + $(this).attr("poi-id")
         };
         //alert(Poi.Id + "; " + Poi.Name + " ; " + Poi.Lat + 3"; " + Poi.Lng);
         PoiList.push(Poi);
-        //alert(Poi.Id + " " + Poi.Name + " " + Poi.Lat + " " + Poi.Lng);
+        //alert(Poi.Id + " " + Poi.Name + " " + Poi.Lat + " " + Poi.Lng + " " + Poi.Url);
     });
 
     PoiList.sort(Comparator);
