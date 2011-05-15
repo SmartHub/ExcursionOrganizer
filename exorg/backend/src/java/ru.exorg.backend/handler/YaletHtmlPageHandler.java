4b525a717e7daceefb79af055ca3128e8b98dcf4
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2011-05-15 15:08:17 +0400

    General performance logging added

--- exorg/backend/src/java/ru.exorg.backend/handler/YaletHtmlPageHandler.java
+++ exorg/backend/src/java/ru.exorg.backend/handler/YaletHtmlPageHandler.java
@@ -5,11 +5,16 @@ import org.eclipse.jetty.server.Request;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.log4j.Logger;
+
 public class YaletHtmlPageHandler extends YaletHandler {
+    private static Logger log = Logger.getLogger("Performance");
+
     private Map<String, String> yalets;
 
     final public void setYaletList(final String[] yl) {
@@ -38,11 +43,17 @@ public class YaletHtmlPageHandler extends YaletHandler {
         if (yalets.containsKey(resName)) {
             if (target.endsWith(".html")) {
                 try {
-                    request.getSession(true);
+                    HttpSession hs = request.getSession(true);
+
+                    long start = System.currentTimeMillis();
+                    log.debug(String.format("Started handling request for %s from %s (%s)", target, request.getRemoteAddr(), hs.getId()));
 
                     String s = String.format(xmlTemplate, resName, this.yalets.get(resName));
                     this.processor.process(request, response, s, true);
                     baseRequest.setHandled(true);
+
+                    long stop = System.currentTimeMillis();
+                    log.debug(String.format("Handling request for %s from %s (%s) finished for %d ms", target, request.getRemoteAddr(), hs.getId(), stop - start));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
