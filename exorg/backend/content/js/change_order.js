11128edad1ab84f0b7f6539c4e33d304ccbfc2be
Pavel Sinay <si---pay@yandex.ru>
2011-05-21 02:44:57 +0400

    changing order

--- exorg/backend/content/js/change_order.js
+++ exorg/backend/content/js/change_order.js
@@ -1,43 +1,133 @@
+var PoiList = [];
+var length = 0;
+
 $(document).ready(
 	function(){
-		$("input.number").bind('change', check);
+		//$("input.number").bind('change', check);
+		loadPoiList();
+		calculate_route(PoiList);
+		//showPoiList(PoiList);
+		//showPoiList(PoiList);
 	}
 );
 
 
-
-function check(){
-	var text = [];
-	var text2 = [];
-	var do_update = true;
-
-	$("input.number").each(
-			function(){
-				$(this).css( "background-color", "white" );
-				text.push(this.value);
-	});
-
-	for(i = 0; i < text.length - 1; ++i){
-		for(j = i + 1; j < text.length; ++j){
-			if(text[i] == text[j]){
-				text2.push(text[i]);
-			}
-		}
-	}
-
-	$("input.number").each(
-			function(){
-				for(i = 0; i < text2.length; ++i){
-					if (this.value == text2[i]){
-						$(this).css( "background-color", "red" );
-						do_update = false;
-					};
-				}
-	});
-
-	if(do_update)
+function loadPoiList() {
+    PoiList.length = 0;
+	$.get('route.html?_ox', {}, function(xml)
 	{
-	    update_points();
-	}
+        $(xml).find('page').find('data').find('route_point').each(function() ///data/route_point
+		{
+			var Poi ={
+				Id : $(this).attr("poi-id"),
+				Order : $(this).attr("order"),
+				Name : $(this).find('name').text(),
+				Lat : $(this).attr("lat"),
+				Lng : $(this).attr("lng"),
+				Url : "poi.html?id=" + $(this).attr("poi-id")
+			};
+			PoiList.push(Poi);
+			//alert(Poi.Name);
+        	});
+        //alert("update fin");
+        showPoiList(PoiList);
+        calculate_route(PoiList);
+        length = PoiList.length;
+	}, 'xml'); // указываем явно тип данных
+}
 
-};
+
+function showPoiList(List) {
+	$('#poi-print').html('');
+
+    s = "<table cellpadding = '10'>";
+	for(var i = 0; i < List.length; ++i)
+	{
+	    s += '<tr><td>';
+
+        s +="<input type='button' value = '^' onClick='up_poi(" +
+		    List[i].Id +
+		    ");' ></input>";
+		s +="<input type='button' value = 'V' onClick='down_poi(" +
+		    List[i].Id +
+		    ");' ></input>";
+
+		s += "<td/><td><a href='poi.html?id=" + List[i].Id + "'>" + List[i].Name + '</a>';
+		s += "<td/><td>ADDRESS<td>";
+
+        s += '<td/><tr/>';
+	};
+
+	s += '<table/>';
+	s += "<input type='button' value = 'Сохранить изменения' onClick='save_order();'></input>";
+
+	$('#poi-print').append(s);
+}
+
+function swap_poi(i, j) {
+
+    Poi = PoiList[i];
+    PoiList[i] = PoiList[j];
+    PoiList[j] = Poi;
+    Order = PoiList[i].Order;
+    PoiList[i].Order = PoiList[j].Order;
+    PoiList[j].Order = Order;
+}
+
+function up_poi(id) {
+    PoiList.length = length;
+    for (i = 1; i < PoiList.length; ++i) {
+        if (PoiList[i].Id == id)
+        {
+            swap_poi(i, i-1);
+            showPoiList(PoiList);
+            calculate_route(PoiList);
+            return;
+        }
+    }
+}
+
+function save_order()
+{
+    s = "&change_order";
+    for (i = 0; i < PoiList.length; ++i) {
+        s += "&" + PoiList[i].Id + "=" + PoiList[i].Order;
+    }
+    	//$.get('constructor.html?_ox' + s, {}, {}, 'xml');
+	$.get('constructor.xml?_ox' + s, {}, function(xml)
+	{
+        $(xml).find('page').find('data').find('route_point').each(function() ///data/route_point
+		{
+			var Poi ={
+				Id : $(this).attr("poi-id"),
+				Order : $(this).attr("order"),
+				Name : $(this).find('name').text(),
+				Lat : $(this).attr("lat"),
+				Lng : $(this).attr("lng"),
+				Url : "poi.html?id=" + $(this).attr("poi-id")
+			};
+			//PoiList.push(Poi);
+			//alert(Poi.Name);
+        	});
+        //alert("update fin");
+        showPoiList(PoiList);
+        calculate_route(PoiList);
+        length = PoiList.length;
+	}, 'xml'); // указываем явно тип данных
+}
+
+function down_poi(id) {
+    PoiList.length = length;
+    for (i = 0; i < PoiList.length-1; ++i) {
+        if (PoiList[i].Id == id)
+        {
+            swap_poi(i, i+1);
+
+            showPoiList(PoiList);
+            calculate_route(PoiList);
+            return;
+        }
+    }
+}
+
+
